521817762de133f2c4762aa3703fe89caf1f48f6
vanelnw <vanelnwaba@gmail.com>
2024-09-18 02:50:35 +0100

    configure and request location authorization

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -1,13 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { FC } from 'react'
+import { Alert, StyleSheet, Text, View } from 'react-native'
+import React, { FC, useEffect } from 'react'
 import {NavigationContainer} from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SplashScreen from '@features/auth/SplashScreen';
+import Geolocation from '@react-native-community/geolocation'
+
+Geolocation.setRNConfiguration({
+    skipPermissionRequests:false,
+    authorizationLevel:'always',
+    enableBackgroundLocationUpdates:true,
+    locationProvider:'auto'
+})
 
 
 const Stack = createNativeStackNavigator()
 
 const Navigation:FC = () => {
+    useEffect(() =>{
+        const fetchUserLocation = async() =>{
+            try {
+                Geolocation.requestAuthorization()
+            } catch (error) {
+                Alert.alert('Sorry we need location service to give you better shopping experience')
+            }
+        }
+        const timoutId = setTimeout(fetchUserLocation,1000);
+        return () => clearTimeout(timoutId)
+    },[]);
   return (
     <NavigationContainer>
       <Stack.Navigator
